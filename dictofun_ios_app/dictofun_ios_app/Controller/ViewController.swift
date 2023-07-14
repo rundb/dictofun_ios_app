1d41d68128510a686f5b5056834e71a2e9cd20eb
Roman Turkin <roma.turkin@gmail.com>
2023-07-14 09:56:49 +0200

    restarted the project, introduced UI-kit based views

---
+++ dictofun_ios_app/dictofun_ios_app/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dictofun_ios_app
+//
+//  Created by Roman on 13.07.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
